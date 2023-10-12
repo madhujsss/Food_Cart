81a2177faa3abd6fcb3b3dc2cd69fa7bc156eba9
madhu.patil <madhu.patil@redbus.com>
2023-10-12 16:00:37 +0530

    add

--- foody/src/App.js
+++ foody/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
@@ -18,6 +18,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <div>
+          Hi 
+        </div>
+      </body>
     </div>
   );
 }
